c77630c9000c2c66bb0436e2f3f27355a34c4c9e
TCDev69 <siforse99@gmail.com>
2024-11-19 00:43:29 +0100

    v7 Transition

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,13 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>
-);  
+);
