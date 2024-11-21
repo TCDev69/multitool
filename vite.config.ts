f21ae9396f5457ce663d7078b4ce9ad4c70a7957
TCDev69 <siforse99@gmail.com>
2024-11-21 11:44:15 +0100

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -15,6 +15,6 @@ export default defineConfig({
   },
   define: {
     'process.env': {},
-    global: 'window', // Impostiamo `global` su `window` per simulare l'ambiente Node.js
+    global: 'window',
   },
 });
